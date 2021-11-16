f6d7cdeee5fc1ceae90717c78e800f153ec1d395
Yaosabi <lorenzoimurillo@outlook.com>
2021-11-16 08:04:15 -0800

    Checkpoint 2

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,11 +1,18 @@
 module.exports.viewAll = function(req, res) {
-    const restaurant = {
+    const restaurant =[ {
         id: 1,
         name: 'Pizza  Hut',
         image: 'https://www.recipetineats.com/wp-content/uploads/2020/05/Pepperoni-Pizza_5-SQjpg.jpg',
         rating: 2,
         category: 'Italian',
         description: 'LMAOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO'
-    };
-    res.render('index', {restaurant});
+    },
+    { id: 2,
+        name: 'some tortellini restaurant',
+        image: 'https://images-gmi-pmc.edge-generalmills.com/a5f63a72-fb76-4549-8a6a-e2fb0ed4671e.jpg',
+        rating: 4,
+        category: 'Italian',
+        description: 'pretty good ngl'
+    }];
+    res.render('index', {restaurants});
 }
