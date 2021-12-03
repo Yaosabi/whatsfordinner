763f4a8c3f0999e22839a8483d095970a6459c6f
Yaosabi <lorenzoimurillo@outlook.com>
2021-12-03 09:46:23 -0800

    checkpoint 6

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,18 +1,5 @@
-module.exports.viewAll = function(req, res) {
-    const restaurant =[ {
-        id: 1,
-        name: 'Pizza  Hut',
-        image: 'https://www.recipetineats.com/wp-content/uploads/2020/05/Pepperoni-Pizza_5-SQjpg.jpg',
-        rating: 2,
-        category: 'Italian',
-        description: 'LMAOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO'
-    },
-    { id: 2,
-        name: 'some tortellini restaurant',
-        image: 'https://images-gmi-pmc.edge-generalmills.com/a5f63a72-fb76-4549-8a6a-e2fb0ed4671e.jpg',
-        rating: 4,
-        category: 'Italian',
-        description: 'pretty good ngl'
-    }];
+const{Restaurant} = require('../models');
+module.exports.viewAll = async function(req, res, next) {
+    const restaurants = await Restaurant.findAll();
     res.render('index', {restaurants});
 }
