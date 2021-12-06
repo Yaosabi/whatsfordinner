4686897b2061c02527a09e0306cab5e9a8e1fa41
Yaosabi <lorenzoimurillo@outlook.com>
2021-12-06 08:08:27 -0800

    Checkpoint 8

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -12,3 +12,55 @@ module.exports.renderEditForm = async function(req, res){
     );
     res.render('edit', {restaurant, categories});
 }
+
+module.exports.updateRestaurant = async function(req,res){
+    await Restaurant.update(
+        {
+            name: req.body.name,
+            category: req.body.category,
+            rating: req.body.rating,
+            image: req.body.image,
+            description: req.body.description
+        },
+        {
+            where:
+                {
+                    id: req.params.id
+                }
+        });
+    res.redirect('/');
+}
+
+module.exports.deleteRestaurant = async function(req, res){
+    await Restaurant.destroy(
+        {
+            where:
+                {
+                    id: req.params.id
+                }
+        });
+    res.redirect('/');
+}
+
+module.exports.renderAddForm = function(req,res){
+    const restaurant = {
+        name: "",
+        description: "",
+        rating: 1,
+        image: "",
+        category: categories[0],
+    };
+    res.render('add', {restaurant, categories});
+}
+
+module.exports.addRestaurant = async function(req, res){
+    await Restaurant.create(
+        {
+            name: req.body.name,
+            category: req.body.category,
+            rating: req.body.rating,
+            image: req.body.image,
+            description: req.body.description
+        });
+    res.redirect('/')
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 const restaurantController = require('../controllers/restaurantController')
 
 /* GET home page. */
-router.get('/', restaurantController.viewAll);
-router.get('/edit/:id', restaurantController.renderEditForm);
 
+router.get('/', restaurantController.viewAll);
+router.get('/edit/:id', restaurantController.updateRestaurant);
+router.get('/delete/:id', restaurantController.deleteRestaurant);
+router.get('/add', restaurantController.renderAddForm);
 module.exports = router;
